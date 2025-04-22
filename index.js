c949fa84131148f0cbd7c76c1abf8925ece87453
Your Name <nguy1601@usherbrooke.ca>
2025-04-22 17:12:14 -0400

    added click event on share link

---
+++ index.js
@@ -0,0 +1,4 @@
+var arrow=document.querySelector(".share-link").addEventListener("click",function(){
+    this.classList.toggle("active");
+    document.querySelector(".share-link-items").classList.toggle("active");
+})
